ffac864dedf5bc759f1de6a54f3ed12e98a8dcfe
VinnyMendess <vinicius.msilva@sptech.school>
2025-12-03 00:38:28 -0300

    Corrigindo bugs e aumentando a dificuldade do Adversario

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
 var dashRouter = require("./src/routes/dash");
 var quizRouter = require("./src/routes/quiz")
 var graphRouter = require("./src/routes/graph")
@@ -30,7 +29,6 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/avisos", avisosRouter);
 app.use("/dash", dashRouter);
 app.use("/quiz", quizRouter);
 app.use("/graph", graphRouter);
